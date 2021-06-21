666f8b51febee60b91069f09be3d599c9c81e603
João Alvarenga <jvalvarenga@mailfence.com>
2021-06-21 15:31:45 -0300

    add updates

--- components/Projects.js
+++ components/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from '../styles/Projects.module.css';
+import Image from 'next/image';
 
 const Projects = () => {
     return ( 
@@ -10,6 +11,16 @@ const Projects = () => {
                         <div className={styles.projects__img}>
                             <div className={styles.projects__content}>
                             <div  className={styles.portifolio__img}>
+                            <span className={styles.glass__wrap}>
+                                <span className={styles.glass__inner__wrap}>
+                                    <h3>nanfbnafujbjufbaujbfujbwfujbwub</h3>
+                                    <span className={styles.icon__container}>
+                                    <Image src="/htmlIcon.svg" width={30} height={30} />
+                                    <Image src="/cssIcon.svg" width={30} height={30} />
+                                    <Image src="/jsIcon.svg" width={30} height={30} />
+                                    </span>
+                                </span>
+                            </span>
                             <img src="/apple-clone.png" width={'100%'} height={'100%'} objectFit='cover' objectPosition='center' />
                             </div>
                                 <span className={styles.portifolio__content__left}><h3>view live</h3></span>
@@ -20,6 +31,16 @@ const Projects = () => {
                         <div className={styles.projects__img__2}>
                             <div className={styles.projects__content__md}>
                             <div  className={styles.portifolio__img}>
+                            <span className={styles.glass__wrap__2}>
+                                <span className={styles.glass__inner__wrap__2}>
+                                    <h3>nanfbnafujbjufbaujbfujbwfujbwub</h3>
+                                    <span className={styles.icon__container__2}>
+                                    <Image src="/htmlIcon.svg" width={30} height={30} />
+                                    <Image src="/cssIcon.svg" width={30} height={30} />
+                                    <Image src="/jsIcon.svg" width={30} height={30} />
+                                    </span>
+                                </span>
+                            </span>
                             <img src="/chillhoop.png" width={'100%'} height={'100%'} objectFit='cover' objectPosition='center' />
                             </div>
                                 <span className={styles.portifolio__content__left}><h3>view live</h3></span>
