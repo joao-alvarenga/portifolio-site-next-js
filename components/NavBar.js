913bc0a09676c38e0149d57af3bc25db26adc24b
João Alvarenga <jvalvarenga@mailfence.com>
2021-06-21 03:17:36 -0300

    add updates

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,10 +3,12 @@ import styles from '../styles/Home.module.css';
 const NavBar = () => {
     return ( 
         <header className={styles.header}>
+      
             <div className={styles.logo__container}>
                 <div className={styles.logo}>
                     <h2>J<span></span>A</h2>
                 </div>
+                <span className={styles.hover__text}><h3>Hi I am João </h3></span>
             </div>
             <nav className={styles.nav}>
                 <ul className={styles.ul}>
