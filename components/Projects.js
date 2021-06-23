30642357412cdc3333a3efda1eb56ee1059aaddf
João Alvarenga <jvalvarenga@mailfence.com>
2021-06-23 15:17:59 -0300

    add updates

--- components/Projects.js
+++ components/Projects.js
@@ -7,43 +7,39 @@ const Projects = () => {
             <div className={styles.projects__container}>
                 <h2 className={styles.portifolio__title}>some of my projects</h2>
             <div className={styles.projects__inner__container}>
-            <div className={styles.portifolio__wrap}>
-                        <div className={styles.projects__img}>
-                            <div className={styles.projects__content}>
-                            <div  className={styles.portifolio__img}>
-                            <span className={styles.glass__wrap}>
-                                <span className={styles.glass__inner__wrap}>
-                                <Image src="/htmlIcon.svg" alt="html logo" width={30} height={30} />
-                                    <Image src="/cssIcon.svg" alt="css logo" width={30} height={30} />
-                                    <Image src="/jsIcon.svg" alt="JavaScript logo" width={30} height={30} />
-                                </span>
-                            </span>
-                            <Image src="/apple-clone.png" alt="portifolio one image" width={1000} height={530} objectFit='cover' objectPosition='center' />
-                            </div>
-                                <span className={styles.portifolio__content__left}><a target="_blank" rel="noopener noreferrer" href="https://joao-alvarenga.github.io/apple-clone/"><h3>view live</h3></a></span>
-                                <span className={styles.portifolio__content__right}><a target="_blank" rel="noopener noreferrer" href="https://github.com/joao-alvarenga/apple-clone"><h3>view code</h3></a></span>
-                            </div>
-                        </div>
-                        </div>
+            <div className={styles.float__left}>
+            <div className={styles.img__wrap}>
+            <div className={styles.img__container}>
+           
+            <Image src="/apple-clone.png" alt="portifolio one image" layout="fill"  objectFit='cover' objectPosition='center' />
+            <span className={styles.hover__text__container}>
+            <Image src="/htmlIcon.svg" alt="html logo" width={30} height={30} />
+            <Image src="/cssIcon.svg" alt="css logo" width={30} height={30} />
+            <Image src="/jsIcon.svg" alt="JavaScript logo" width={30} height={30} />
+            </span>
+            </div>
+            <span className={styles.projects__btn__left}><a target="_blank" rel="noopener noreferrer" href="https://joao-alvarenga.github.io/apple-clone/"><h3>view live</h3></a></span>
+            <span className={styles.projects__btn__right}><a target="_blank" rel="noopener noreferrer" href="https://github.com/joao-alvarenga/apple-clone"><h3>view code</h3></a></span>
+            </div>
+            </div>
+
+            <div className={styles.float__right}>
+            <div className={styles.img__wrap}>
+            <div className={styles.img__container}>
+            <Image src="/chillhoop.png" alt="portifolio two image" layout="fill" objectFit='cover' objectPosition='center' />
+            <span className={styles.hover__text__container}>
+            <Image src="/htmlIcon.svg" alt="html logo" width={30} height={30} />
+            <Image src="/sassIcon.svg" alt="Sass logo" width={30} height={30} />
+            <Image src="/jsIcon.svg" alt="JavaScript logo" width={30} height={30} />
+            </span>
+            </div>
+            <span className={styles.projects__btn__left}><a target="_blank" rel="noopener noreferrer" href="https://chillhoop-music-player.web.app/"><h3>view live</h3></a></span>
+            <span className={styles.projects__btn__right}><a target="_blank" rel="noopener noreferrer" href="https://github.com/joao-alvarenga/music-player-app"><h3>view code</h3></a></span>
+            </div>
+            </div>
+
                    
-                        <div className={styles.portifolio__wrap__md}>
-                        <div className={styles.projects__img__2}>
-                            <div className={styles.projects__content__md}>
-                            <div  className={styles.portifolio__img}>
-                            <span className={styles.glass__wrap__2}>
-                                <span className={styles.glass__inner__wrap__2}>
-                                <Image src="/htmlIcon.svg" alt="html logo" width={30} height={30} />
-                                    <Image src="/sassIcon.svg" alt="Sass logo" width={30} height={30} />
-                                    <Image src="/jsIcon.svg" alt="JavaScript logo" width={30} height={30} />
-                                </span>
-                            </span>
-                            <Image src="/chillhoop.png" alt="portifolio two image" width={1000} height={530} objectFit='cover' objectPosition='center' />
-                            </div>
-                                <span className={styles.portifolio__content__left}><a target="_blank" rel="noopener noreferrer" href="https://chillhoop-music-player.web.app/"><h3>view live</h3></a></span>
-                                <span className={styles.portifolio__content__right}><a target="_blank" rel="noopener noreferrer" href="https://github.com/joao-alvarenga/music-player-app"><h3>view code</h3></a></span>
-                            </div>
-                        </div>
-                        </div>
+                     
             </div>
             </div>
         </section>
