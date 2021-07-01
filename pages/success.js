571508af624249942d13b509a51bb3ec1f00dd8b
João Alvarenga <jvalvarenga@mailfence.com>
2021-07-01 16:28:49 -0300

    add updates

--- pages/success.js
+++ pages/success.js
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import Link from 'next/link';
 import styles from '../styles/Success.module.css';
 
 const Success = () => {
@@ -8,7 +8,7 @@ const Success = () => {
                 <h1 className={styles.succsess__title}>
                     Message received. Thank you!
                 </h1>
-              <Link href="https://joaoalvarenga-me.vercel.app/"><button className={styles.succsess__btn}>
+              <Link passhref="https://joaoalvarenga-me.vercel.app/"><button className={styles.succsess__btn}>
                     <h2>back to home</h2>
                 </button></Link>
             </main>
